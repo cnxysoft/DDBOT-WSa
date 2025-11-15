323b5c37cd374680489abf9d7abed2027e8bb576
Alen <cnxysoft@126.com>
2025-11-16 04:02:58 +0800

    更新网络请求默认UA

--- requests/requests.go
+++ requests/requests.go
@@ -26,7 +26,7 @@ type RespHeader struct {
 
 var logger = utils.GetModuleLogger("request")
 
-var defaultUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+var defaultUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0"
 
 type option struct {
 	Timeout            time.Duration
