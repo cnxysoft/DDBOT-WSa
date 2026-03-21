432ecd0b5cf4a6ac87131d48ad222b3b618c70d3
Alen <cnxysoft@126.com>
2026-03-21 21:23:07 +0800

    StateManager: 为 EmitQueue 添加站点级别的 interval 配置支持
    - 新增 GetEmitIntervalForSite(site string) 函数，支持站点自定义间隔
    - 新增 UseEmitQueueWithSiteInterval(site string) 方法
    - 优先级：site.interval > concern.emitInterval > 默认值 5s

--- lsp/cfg/config.go
+++ lsp/cfg/config.go
@@ -85,6 +85,18 @@ func GetEmitInterval() time.Duration {
 	return config.GlobalConfig.GetDuration("concern.emitInterval")
 }
 
+// GetEmitIntervalForSite 获取站点级别的 emit interval
+// 优先级：site.interval > concern.emitInterval > 默认值 5s
+func GetEmitIntervalForSite(site string) time.Duration {
+	// 先尝试站点配置
+	interval := config.GlobalConfig.GetDuration(site + ".interval")
+	if interval > 0 {
+		return interval
+	}
+	// 返回全局配置
+	return GetEmitInterval()
+}
+
 func GetLargeNotifyLimit() int {
 	var limit = config.GlobalConfig.GetInt("dispatch.largeNotifyLimit")
 	if limit <= 0 {
